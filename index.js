04a2d19cbc677f6cf279fd10de3a75631682ace3
Pavlina Glozygova <pglozygova@gmail.com>
2023-05-07 18:46:21 +0200

    modified:   index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('funguju!');
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
